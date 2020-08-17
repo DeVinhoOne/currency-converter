fb5f86acaa2bbdc91377dfe0a109e161c84758e9
dawid <caushered@gmail.com>
2020-08-17 15:55:11 +0200

    get input amount and calc the result

--- src/js/currency-converter.js
+++ src/js/currency-converter.js
@@ -5,9 +5,14 @@ form.addEventListener('submit', (e) => convertCurrency(e))
 const convertCurrency = (e) => {
    e.preventDefault()
 
+   const amount = form.querySelector('#amount').value
    const base = form.querySelector('#from').value
    const to = form.querySelector('#to').value
    fetch(`https://api.exchangeratesapi.io/latest?base=${base}`)
       .then((resposne) => resposne.json())
-      .then((data) => console.log(data.rates[to]))
+      .then((data) => {
+         const rate = data.rates[to]
+         const result = amount * rate
+         console.log(result.toFixed(2))
+      })
 }
