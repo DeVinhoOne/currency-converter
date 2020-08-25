7be81c1fea96e684d2e45dd580647f206094be07
dawid <caushered@gmail.com>
2020-08-25 11:38:52 +0200

    animate summary on first print

--- src/js/print-result.js
+++ src/js/print-result.js
@@ -5,7 +5,11 @@ export const printResult = (amount, base, to, result) => {
    const summaryResult = summary.querySelector('.summary__result')
    const summaryArrow = summary.querySelector('.summary__arrow')
 
+   // Print to the screen
    summaryAmount.textContent = `${amount} ${base}`
-   summaryArrow.style.display = 'block'
    summaryResult.textContent = `${result} ${to}`
+   summaryArrow.style.display = 'block'
+   summary.style.opacity = '1'
+   summary.style.transform = 'translate(0, 0)'
+   summaryArrow.style.opacity = '1'
 }
